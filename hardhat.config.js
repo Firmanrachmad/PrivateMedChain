2b9074685e6d15b83f2fd0e6db767715b6cbf3d7
Firmanrachmad <dragobniel@gmail.com>
2023-05-09 08:31:24 +0700

    setup hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  }
 };
