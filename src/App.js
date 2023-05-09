e28c92007c97c69b245efbed0a0614b0d9dcc03a
Firmanrachmad <dragobniel@gmail.com>
2023-05-09 13:03:00 +0700

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React-Testing Git testing
+          Learn React
         </a>
       </header>
     </div>
