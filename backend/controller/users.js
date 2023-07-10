daf766aa22142e55dcc7248a0b515ed5427482a1
Firmanrachmad <dragobniel@gmail.com>
2023-07-11 03:07:58 +0700

    update auth & logout

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -3,8 +3,22 @@ const User = require("../models/Users");
 const {generateToken} = require("../utils/generateToken");
 
 const authUser = asyncWrapper(async (req, res) => {
-    
-    res.status(200).json({message: 'Auth User'});
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid email or password');
+    }
+
 });
 
 const registerUser = asyncWrapper(async (req,res) => {
@@ -34,12 +48,14 @@ const registerUser = asyncWrapper(async (req,res) => {
         res.status(400);
         throw new Error('Invalid user data');
     }
-
-    // res.status(200).json({message: 'Register User'});
 });
 
 const logoutUser = asyncWrapper(async(req, res) => {
-    res.status(200).json({message: 'Logout User'});
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message: 'User logged out'});
 });
 
 const getUserProfile = asyncWrapper(async(req, res) => {
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -28,4 +28,8 @@ userSchema.pre('save', async function (next) {
     this.password = await bcrypt.hash(this.password, salt);
 });
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
 module.exports = mongoose.model("users", userSchema);
