5c8603c0e10a246b746bd5b4a0d847ec56e50d72
Firmanrachmad <dragobniel@gmail.com>
2023-07-17 06:57:28 +0700

    add rbac

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,9 +9,9 @@ const protect = asyncWrapper(async (req, res, next) => {
 
   if (token) {
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.userId).select('-password');
-        next();
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
     } catch (error) {
       res.status(401);
       throw new Error("Not Authorized, invalid token");
@@ -22,4 +22,54 @@ const protect = asyncWrapper(async (req, res, next) => {
   }
 });
 
-module.exports = { protect };
+const checkPRKRoles = asyncWrapper(async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      if (req.user.roles == "PRK") {
+        next();
+      } else {
+        res.status(403);
+        throw new Error("Not Authorized, Only PRK Can Access This");
+      }
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not Authorized, Only PRK Can Access This");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+});
+
+const checkTkRoles = asyncWrapper(async (req, res, next) => {
+  let token;
+
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      if (req.user.roles == "TK") {
+        next();
+      } else {
+        res.status(403);
+        throw new Error("Not Authorized, Only TK Can Access This");
+      }
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not Authorized, Only TK Can Access This");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+});
+
+module.exports = { protect, checkPRKRoles, checkTkRoles };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/authMiddleware");
+const { protect, checkPRKRoles, checkTkRoles } = require("../middleware/authMiddleware");
 
 const {
   authUser,
@@ -20,8 +20,8 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route("/allusers").get(getAllUser);
-router.route("/delete/:id").delete(protect, deleteUser);
-router.route("/update/:id").put(protect, updateUserId);
+router.route("/allusers").get(checkPRKRoles, getAllUser);
+router.route("/delete/:id").delete(checkPRKRoles, deleteUser);
+router.route("/update/:id").put(checkPRKRoles, updateUserId);
 
 module.exports = router;
